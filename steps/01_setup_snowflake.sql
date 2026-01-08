4b307306f6650c83f8e1e8062a42f6cc1500131d
fauzan <fauzanalagung@gmail.com>
2026-01-08 09:40:16 +0000

    parametrize pipeline

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -23,7 +23,10 @@ CREATE OR REPLACE GIT REPOSITORY quickstart_common.public.quickstart_repo
   ORIGIN = 'https://github.com/fauzan-ucng/sfguide-getting-started-with-snowflake-devops_vscode_local.git'; -- INSERT URL OF FORKED REPO HERE
 
 
-CREATE OR ALTER DATABASE QUICKSTART_PROD;
+-- CREATE OR ALTER DATABASE QUICKSTART_PROD;
+CREATE OR ALTER DATABASE QUICKSTART_{{environment}}; 
+
+
 
 
 -- To monitor data pipeline's completion
